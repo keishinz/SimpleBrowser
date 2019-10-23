a500a89827ac421b1dc44ccf5b75da70fae62138
kesihinz <keishin.chou@gmail.com>
2019-10-23 20:52:51 +0900

    Initial Commit

---
+++ SimpleBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleBrowser
+//
+//  Created by Keishin CHOU on 2019/10/23.
+//  Copyright © 2019 Keishin CHOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
